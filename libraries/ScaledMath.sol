06d728b8e8b88f59f9f481a84376d78a6ec356a4
Cedric Cordenier <cedric@gyro.finance>
2022-11-29 10:39:21 +0000

    Add ScaledMath library to main trunk

---
+++ libraries/ScaledMath.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.15;
+
+library ScaledMath {
+    uint256 internal constant ONE = 1e18;
+
+    function mulDown(uint256 a, uint256 b) internal pure returns (uint256) {
+        return (a * b) / ONE;
+    }
+
+    function divDown(uint256 a, uint256 b) internal pure returns (uint256) {
+        return (a * ONE) / b;
+    }
+}
