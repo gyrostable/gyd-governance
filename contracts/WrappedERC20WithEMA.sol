dbb48bc87db0a0095cb961292c518c3be7e5f739
Daniel Perez <daniel@perez.sh>
2023-03-30 22:07:23 +0100

    Fix issues in EMA
    CU-866a07z17[complete]
    CU-866a07z18[complete]
    CU-866a07z1e[complete]

--- contracts/WrappedERC20WithEMA.sol
+++ contracts/WrappedERC20WithEMA.sol
@@ -67,13 +67,20 @@ contract WrappedERC20WithEMA is IWrappedERC20WithEMA, ERC20, GovernanceOnly {
             uint256 deltaBlockNb = (previousWrappedPctOfSupply.blockNb -
                 expMovingAverage.blockNb) * ScaledMath.ONE;
             int256 exponent = -int256(deltaBlockNb.divDown(windowWidth));
-            expMovingAverage.value += uint256(
-                ((int256(ScaledMath.ONE) - LogExpMath.exp(exponent)) *
-                    int256(
-                        previousWrappedPctOfSupply.value -
-                            expMovingAverage.value
-                    )) / int256(ScaledMath.ONE)
-            );
+            uint256 multiplier = ScaledMath.ONE;
+            if (exponent > LogExpMath.MIN_NATURAL_EXPONENT) {
+                int256 discount = LogExpMath.exp(exponent);
+                multiplier -= discount < 0 ? 0 : uint256(discount);
+            }
+
+            if (previousWrappedPctOfSupply.value > expMovingAverage.value) {
+                expMovingAverage.value += (previousWrappedPctOfSupply.value -
+                    expMovingAverage.value).mulDown(multiplier);
+            } else {
+                expMovingAverage.value -= (expMovingAverage.value -
+                    previousWrappedPctOfSupply.value).mulDown(multiplier);
+            }
+
             expMovingAverage.blockNb = previousWrappedPctOfSupply.blockNb;
         }
         previousWrappedPctOfSupply.value = wrappedPctOfSupply();
