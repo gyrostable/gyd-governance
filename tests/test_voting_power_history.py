14b4375367c70d0a970554ff5972bac114630340
Daniel Perez <daniel@perez.sh>
2023-06-12 14:30:25 +0100

    Fix voting power history tests

--- tests/test_voting_power_history.py
+++ tests/test_voting_power_history.py
@@ -9,7 +9,7 @@ def voting_power_history(admin, VotingPowerHistoryLibrary):
 
 def test_voting_power_history(admin, voting_power_history):
     vph = voting_power_history
-    tx = vph.updateVotingPower(admin, 1e18, 0e18, 0e18)
+    tx = vph.updateVotingPower(admin, 1e18, 1e18, 0e18)
     assert vph.getVotingPower(admin, tx.timestamp) == 1e18
 
     tx = vph.updateVotingPower(admin, 1e18, 3e18, 1e18)
@@ -21,44 +21,44 @@ def test_voting_power_history(admin, voting_power_history):
 
 def test_binary_search(admin, voting_power_history):
     vph = voting_power_history
-    tx1 = vph.updateVotingPower(admin, 1e18, 0e18, 0e18)
+    tx1 = vph.updateVotingPower(admin, 1e18, 1e18, 0e18)
 
     # check the binary search when tx1.timestamp is at the boundary of a record
     assert vph.binarySearch(admin, tx1.timestamp) == (
         True,
-        (tx1.timestamp, 1e18, 1e18, 0e18),  # multiplier is set to 1 if zero
+        (tx1.timestamp, 1e18, 1e18, 0e18),
     )
 
     # check when the record is before the timestamp
     assert vph.binarySearch(admin, tx1.timestamp + 10) == (
         True,
-        (tx1.timestamp, 1e18, 1e18, 0e18),  # multiplier is set to 1 if zero
+        (tx1.timestamp, 1e18, 1e18, 0e18),
     )
 
     # check when the record is after the timestamp
     assert vph.binarySearch(admin, tx1.timestamp - 10) == (False, (0, 0, 1e18, 0))
 
     chain.sleep(2)
-    tx2 = vph.updateVotingPower(admin, 2e18, 0e18, 0e18)
+    tx2 = vph.updateVotingPower(admin, 2e18, 1e18, 0e18)
 
     # check when the timestamp is between tx1 and tx2
     assert vph.binarySearch(admin, tx1.timestamp + 1) == (
         True,
-        (tx1.timestamp, 1e18, 1e18, 0e18),  # multiplier is set to 1 if zero
+        (tx1.timestamp, 1e18, 1e18, 0e18),
     )
 
     # check when the timestamp is after tx2
     assert vph.binarySearch(admin, tx2.timestamp + 10) == (
         True,
-        (tx2.timestamp, 2e18, 1e18, 0e18),  # multiplier is set to 1 if zero
+        (tx2.timestamp, 2e18, 1e18, 0e18),
     )
 
     chain.sleep(2)
-    tx3 = vph.updateVotingPower(admin, 3e18, 0e18, 0e18)
+    tx3 = vph.updateVotingPower(admin, 3e18, 1e18, 0e18)
     # check when the timestamp is after tx3
     assert vph.binarySearch(admin, tx3.timestamp + 10) == (
         True,
-        (tx3.timestamp, 3e18, 1e18, 0e18),  # multiplier is set to 1 if zero
+        (tx3.timestamp, 3e18, 1e18, 0e18),
     )
 
     # check when the timestamp is before tx1
