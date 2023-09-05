2c8c3cd66b7e6b612dfa116203b815d2fcda16cd
Daniel Perez <daniel@perez.sh>
2023-09-05 19:07:50 +0100

    Deploy mock councillor vault to polygon

--- scripts/proof.ts
+++ scripts/proof.ts
@@ -1,25 +1,45 @@
 import { utils } from "ethers";
-import { promises } from "fs";
+import fs from "fs/promises";
 import path from "path";
 
-const { open, readFile } = promises;
+const defaultInFile = path.join(path.dirname(__dirname), "data", "discord_contributors.csv");
+const defaultOutFile = path.join(path.dirname(__dirname), "data", "proofs-discord.json");
 
-const defaultInFile = path.join(path.dirname(__dirname), "data", "discord_contributors.csv")
-const defaultOutFile = path.join(path.dirname(__dirname), "data", "proofs-discord.json")
+class Element {
+  owner: string;
+  multiplier?: BigInt;
 
-type Element = { owner: string, multiplier: BigInt }
+  constructor(owner: string, multiplier?: BigInt) {
+    this.owner = owner;
+    this.multiplier = multiplier;
+  }
+
+  toLeaf() {
+    if (this.multiplier) {
+      return utils.solidityKeccak256(["address", "uint128"], [this.owner, this.multiplier]);
+    } else {
+      return utils.solidityKeccak256(["address"], [this.owner]);
+    }
+  }
+
+  toObject(): { owner: string; multiplier?: string } {
+    return { owner: this.owner, ...(this.multiplier && { multiplier: this.multiplier.toString() }) };
+  }
+
+  static fromLine(line: string): Element {
+    const [owner, multiplier] = line.split(",");
+    return new Element(owner, multiplier ? BigInt(parseInt(multiplier)) : undefined);
+  }
+}
+
 type OutputDatum = {
-  owner: string,
-  multiplier: string,
-  proof: string[]
+  owner: string;
+  multiplier?: string;
+  proof: string[];
 };
 
-
-function traverseMerkle(
-  elements: Element[],
-  process: ((leaves: string[]) => void) | undefined = undefined,
-): string {
-  let leaves = elements.map((a) => utils.solidityKeccak256(["address", "uint128"], [a.owner, a.multiplier]));
+function traverseMerkle(elements: Element[], process: ((leaves: string[]) => void) | undefined = undefined): string {
+  let leaves = elements.map((e) => e.toLeaf());
   while (leaves.length > 1) {
     if (leaves.length % 2 === 1) {
       leaves.push(utils.solidityKeccak256([], []));
@@ -51,7 +71,7 @@ function generateProof(line: Element, lines: Element[]): string[] {
 
   const process = (leaves: string[]) => {
     const delta = nodeIndex % 2 === 0 ? 1 : -1;
-    const hash = leaves[nodeIndex+delta]
+    const hash = leaves[nodeIndex + delta];
     hashes.push(hash);
     nodeIndex = Math.floor(nodeIndex / 2);
   };
@@ -62,42 +82,36 @@ function generateProof(line: Element, lines: Element[]): string[] {
 }
 
 async function generateProofs(inFile: string, outFile: string): Promise<void> {
-  const ownerData = await readFile(inFile, "utf-8");
-  const lines = ownerData.trim().split("\n")
-  const parse = (line: string) => {
-   const parsed = line.split(",");
-   return { owner: parsed[0], multiplier: BigInt(parseInt(parsed[1])) };
-  }
-  const data = lines.map(parse);
+  console.log(inFile, outFile);
+  const ownerData = await fs.readFile(inFile, "utf-8");
+  const lines = ownerData.trim().split("\n");
+  const data = lines.map(Element.fromLine);
 
   const root = generateRoot(data);
   const proofs: OutputDatum[] = [];
   for (const datum of data) {
     const proof = generateProof(datum, data);
-    let result = validateProof(datum.owner, datum.multiplier, proof, root);
+    let result = validateProof(datum, proof, root);
     if (!result) {
-	    throw "invalid proof"
+      throw "invalid proof";
     }
-    proofs.push({
-      owner: datum.owner,
-      multiplier: datum.multiplier.toString(),
-      proof
-    });
+    proofs.push({ ...datum.toObject(), proof });
   }
 
-  const out = await open(outFile, 'w');
-  out.writeFile(JSON.stringify({ root: generateRoot(data), proofs: proofs}));
+  const output = JSON.stringify({ root: generateRoot(data), proofs: proofs });
+  await fs.writeFile(outFile, output, "utf-8");
 }
 
-function validateProof(owner: string, multiplier: BigInt, proof: string[], root: string): boolean {
-  let node = utils.solidityKeccak256(["address", "uint128"], [owner, multiplier]);
+function validateProof(element: Element, proof: string[], root: string): boolean {
+  let node = element.toLeaf();
   for (const proofElement of proof) {
-      let [left, right] = [node, proofElement];
-      if (left > right) [left, right] = [right, left];
-      node = utils.solidityKeccak256(["bytes32", "bytes32"], [left, right]);
+    let [left, right] = [node, proofElement];
+    if (left > right) [left, right] = [right, left];
+    node = utils.solidityKeccak256(["bytes32", "bytes32"], [left, right]);
   }
-  return node == root
+  return node == root;
 }
 
+const [, , inFile, outFile] = process.argv;
 
-generateProofs(defaultInFile, defaultOutFile);
+generateProofs(inFile || defaultInFile, outFile || defaultOutFile);
